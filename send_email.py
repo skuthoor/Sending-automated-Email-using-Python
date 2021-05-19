4d9bfe870372b2f32cde1e64fd539a534d79c957
skuthoor <sanjay109kuthoor@gmail.com>
2021-05-19 15:45:56 +0530

    send email

---
+++ send_email.py
@@ -0,0 +1,15 @@
+#import pandas as pd 
+import smtplib
+#e = pd.read_excel("email.xlsx")
+#emails = e['emails'].values
+emails = ['sanjay109kuthoor@gmail.com','s109kuthoor@gmail.com']
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login("sanjay.p@ug.cusat.ac.in","sanju123")
+msg = 'hello checking the script'
+subject ='Hello There!'
+body="subject:{}\n\n{}".format(subject,msg)
+for email in emails :
+    server.sendmail("sanjay.p@ug.cusat.ac.in",email,body)
+
+server.quit()
