74a2f9ac1daf29b3a48000ab74f63f6fc6b2a52e
Sanjaypk <56498951+skuthoor@users.noreply.github.com>
2021-08-02 17:33:17 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ import smtplib
 emails = ['sanjay109kuthoor@gmail.com','s109kuthoor@gmail.com']
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
-server.login("sanjay.p@ug.cusat.ac.in","sanju123")
+server.login("sanjay.p@ug.cusat.ac.in","*****")
 msg = 'hello checking the script'
 subject ='Hello There!'
 body="subject:{}\n\n{}".format(subject,msg)
